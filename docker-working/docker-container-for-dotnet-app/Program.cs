e6ea00ed7c1c49884363142aedfaf05b18517afb
arbems@outlook.com <arbems@outlook.com>
2023-03-11 20:15:26 +0100

    Add docker-working

---
+++ docker-working/docker-container-for-dotnet-app/Program.cs
@@ -0,0 +1,9 @@
+﻿var sum = 0;
+var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+while (max is -1 || sum < max)
+{
+    int num = new Random().Next(1, 10);
+
+    Console.WriteLine($"Suma {sum} + {num} = {sum += num}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
